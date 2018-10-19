f187d2c2fea153497ce306b94fb90d71f78e0933
Valle Benoît <benoit.valleea@hotmail.fr>
2018-10-19 10:45:42 +0200

    ajout de config.php et du gitignore de variable d'environnement

---
+++ config.php
@@ -0,0 +1,17 @@
+
+
+<?php
+
+function dbConnect() 
+        {
+            try
+            {
+                $db = new PDO("mysql:host=localhost;dbname=$db;charset=utf8","$user","$password");
+                return $db;
+            }
+            catch(Exception $e)
+            {
+            die("Base de données non trouvée");
+            }
+        
+        }
