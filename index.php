5d3064e31238c46b5c61055f25c380f0130899bd
Alex Kaprielian <alex.kaprielian@gmail.com>
2018-10-20 16:57:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,10 @@
 
 
 session_start();
-<<<<<<< HEAD
-    require 'Model/connex_db.php';
-=======
 
-=======
->>>>>>> 15ec490419502e5f39508b1b89d1c7d181907cb2
+    require 'Model/connex_db.php';
+
+
 
 define('WEBROOT', dirname(__FILE__));
 define('BASE_URL', dirname($_SERVER['SCRIPT_NAME']));
