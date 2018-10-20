00d2bbc24e78411523cfe756758cf0335ec47533
Alex Kaprielian <alex.kaprielian@gmail.com>
2018-10-20 17:55:00 +0200

    pour faire remarcher le routeur

--- Model/connex_db.php
+++ Model/connex_db.php
@@ -2,10 +2,7 @@
 
     require'variables-environnement.php';
 
-    echo $db ;
-    echo $user ;
-    echo $password ;
-
+    
             try
             {
                 $db = new PDO("mysql:host=localhost;dbname=$db;charset=utf8","$user","$password");
--- View/header.php
+++ View/header.php
@@ -24,42 +24,8 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <?php
-        if(isset($_SESSION['connecte']))
-        {
-          ?>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
 
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="<?=BASE_URL;?>/">Mon Compte</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="<?=BASE_URL;?>/controller_logout">Se Deconnecter</a>
-            </li>
-
-            <?php
-              if($_SESSION['niveau'] == 3)
-              {
-              ?>
-            <li class="nav-item">
-              <a class="nav-link" href="<?=BASE_URL;?>/">Gestion Admin</a>
-            </li>
-            <?php
-          }
-        }
-        else {
-          ?>
-          <li class="nav-item">
-            <?php require "Controller/controller_inscription.php;"?>
-          </li>
-          <li class="nav-item">
-            <?php require "Controller/controller_connexion.php;"?>
-          </li>
-        <?php } ?>
-
+        
           </ul>
         </div>
       </div>
